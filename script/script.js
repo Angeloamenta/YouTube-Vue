d9329c9a9899a84d178ff2a8934e3d7a4e4298dd
Angeloamenta <angeloamenta67@gmail.com>
2023-02-15 22:19:17 +0100

    shorts

--- script/script.js
+++ script/script.js
@@ -3,7 +3,135 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      message: 'Hello Vue!'
+      message: 'Hello Vue!',
+      videocards: [
+        {
+            imgvideo:"video-1.jpg",
+            title:"Titolo video",
+            canale: "nome canale"
+
+        },
+
+        {
+            imgvideo:"video-1.jpg",
+            title:"Titolo video",
+            canale: "nome canale"
+
+        },
+        {
+            imgvideo:"video-1.jpg",
+            title:"Titolo video",
+            canale: "nome canale"
+
+        },
+        {
+            imgvideo:"video-1.jpg",
+            title:"Titolo video",
+            canale: "nome canale"
+
+        },
+        {
+            imgvideo:"video-1.jpg",
+            title:"Titolo video",
+            canale: "nome canale"
+
+        },
+        {
+            imgvideo:"video-1.jpg",
+            title:"Titolo video",
+            canale: "nome canale"
+
+        },
+        {
+            imgvideo:"video-1.jpg",
+            title:"Titolo video",
+            canale: "nome canale"
+
+        },
+
+      ],
+
+      shorts: [
+
+        {
+          img:"short.jpg",
+          title:"Titolo",
+          visual:1234567
+        },
+        {
+          img:"short.jpg",
+          title:"Titolo",
+          visual:1234567
+        },
+        {
+          img:"short.jpg",
+          title:"Titolo",
+          visual:1234567
+        },
+        {
+          img:"short.jpg",
+          title:"Titolo",
+          visual:1234567
+        },
+        {
+          img:"short.jpg",
+          title:"Titolo",
+          visual:1234567
+        },
+        {
+          img:"short.jpg",
+          title:"Titolo",
+          visual:1234567
+        },
+        {
+          img:"short.jpg",
+          title:"Titolo",
+          visual:1234567
+        },
+        {
+          img:"short.jpg",
+          title:"Titolo",
+          visual:1234567
+        },
+        {
+          img:"short.jpg",
+          title:"Titolo",
+          visual:1234567
+        },
+        {
+          img:"short.jpg",
+          title:"Titolo",
+          visual:1234567
+        },
+
+      ],
+
+      videos: [
+        {
+            imgvideo:"video-1.jpg",
+            title:"Titolo video",
+            canale: "nome canale"
+
+        },
+        {
+            imgvideo:"video-1.jpg",
+            title:"Titolo video",
+            canale: "nome canale"
+
+        },
+        {
+            imgvideo:"video-1.jpg",
+            title:"Titolo video",
+            canale: "nome canale"
+
+        },
+        {
+            imgvideo:"video-1.jpg",
+            title:"Titolo video",
+            canale: "nome canale"
+
+        },
+      ]
     }
   }
 }).mount('#app')
